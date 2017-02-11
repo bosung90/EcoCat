91c0c6a283dd443a003781ff89b61d8ad7b24697
Eric Kim <bosung90@gmail.com>
2017-02-11 11:33:46 -0800

    Added EcoCat (created Prefab) EcoCat Animation.

---
+++ Assets/EcoCat/Scripts/EcoCat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcoCat : MonoBehaviour {
+
+//	private Animator animator;
+//
+//	void Awake() {
+//		animator = GetComponent<Animator> ();
+//	}
+//
+//	void Start() {
+//		animator.get
+//	}
+
+}
