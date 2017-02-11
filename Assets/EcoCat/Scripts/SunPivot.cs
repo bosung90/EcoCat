3fde29b0469c551c8c35615b6b0e2ac770219311
Tracy Wong <tracyryna@gmail.com>
2017-02-11 13:46:03 -0800

    Added SunPivot Script. Sun will now set during the game play.

---
+++ Assets/EcoCat/Scripts/SunPivot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunPivot : MonoBehaviour {
+
+	private RectTransform rectTransform;
+	private float sunZ = 10f;
+
+	void Awake() {
+		rectTransform = GetComponent<RectTransform> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sunZ = sunZ - Time.deltaTime;
+		rectTransform.rotation = Quaternion.Euler (new Vector3 (0, 0, sunZ));
+	}
+}
