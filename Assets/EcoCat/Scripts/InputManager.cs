4295e12656c6d7889c56d68ecb06ded50fec2d8a
Eric Kim <bosung90@gmail.com>
2017-02-11 11:06:36 -0800

    Added images. Added InputManager with Jump observable

---
+++ Assets/EcoCat/Scripts/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class InputManager : MonoBehaviour {
+
+	public static InputManager Instance;
+
+	private Subject<Unit> jump = new Subject<Unit>();
+	public IObservable<Unit> Jump;
+
+	void Awake() {
+		Instance = this;
+		Jump = Observable.EveryUpdate ().Where (_ => Input.GetKeyDown (KeyCode.Space)).AsUnitObservable ();
+	}
+
+
+}
