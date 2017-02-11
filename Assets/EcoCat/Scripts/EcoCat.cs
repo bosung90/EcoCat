fc494d1a1fb820a9aa1979d60f421b2e75cc2aa0
Tracy Wong <tracyryna@gmail.com>
2017-02-11 12:37:57 -0800

    Cat can jump and is more realistically sized

--- Assets/EcoCat/Scripts/EcoCat.cs
+++ Assets/EcoCat/Scripts/EcoCat.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class EcoCat : MonoBehaviour {
 
+	private Rigidbody2D rigidBody2D;
+
+	void Awake() {
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start() {
+		InputManager.Instance.Jump.Subscribe (_ => {
+			var originalVelocity = rigidBody2D.velocity;
+			rigidBody2D.velocity = new Vector2(originalVelocity.x, 2.5f);
+		}).AddTo(this);
+	}
 //	private Animator animator;
 //
 //	void Awake() {
