18d9af0e3328d7bd195753a7f10a92f6aee56fc8
Eric Kim <bosung90@gmail.com>
2017-02-12 15:36:09 -0800

    Wave comes up when carbon level is full

---
+++ Assets/EcoCat/Scripts/WavePivot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class WavePivot : MonoBehaviour {
+
+	void Start () {
+		CarbonLevel.Instance.carbonLevelFull.DistinctUntilChanged().Subscribe (isFull => {
+			if(isFull) {
+				var pos = this.transform.position;
+				pos.y = 0.35f;
+				this.transform.position = pos;
+			} else {
+				var pos = this.transform.position;
+				pos.y = 0f;
+				this.transform.position = pos;
+			}
+		}).AddTo (this);
+	}
+
+}
