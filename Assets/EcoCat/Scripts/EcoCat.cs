3b454aacb5b5efbe999bd308145ee19b08c86b80
brian <brianhylee99@gmail.com>
2017-02-11 17:40:53 -0800

    water bottle generator (buggy!)

--- Assets/EcoCat/Scripts/EcoCat.cs
+++ Assets/EcoCat/Scripts/EcoCat.cs
@@ -6,6 +6,8 @@ using UniRx;
 public class EcoCat : MonoBehaviour {
 
 	private Rigidbody2D rigidBody2D;
+    private bool facingRight;
+    //private float maxSpeed = 1.0f;
 
 	void Awake() {
 		rigidBody2D = GetComponent<Rigidbody2D> ();
@@ -20,14 +22,44 @@ public class EcoCat : MonoBehaviour {
 		InputManager.Instance.HorizontalForce.Subscribe (force => {
 			rigidBody2D.AddForce(Vector2.right * force * 8);
 		}).AddTo (this);
+
+        facingRight = true;
+
 	}
-//	private Animator animator;
-//
-//	void Awake() {
-//		animator = GetComponent<Animator> ();
-//	}
-//
-//	void Start() {
+
+    void Update()
+    {
+        // Check the velocity of the cat, and if the direction faced =/= velocity
+        // then call Flip()
+        Vector2 vel = rigidBody2D.velocity;
+        if (vel.x < 0 && facingRight)
+        {
+            Flip();
+        } else if (vel.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        
+    }
+
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        facingRight = !facingRight;
+    }
+
+    //	private Animator animator;
+    //
+    //	void Awake() {
+    //		animator = GetComponent<Animator> ();
+    //	}
+    //
+    //	void Start() {
+    //		animator.get
+
 //		animator.get
 //	}
 
---
+++ Assets/EcoCat/Scripts/RecyclableGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecyclableGenerator : MonoBehaviour {
+
+    public int count = 0;
+    private int maxCount = 5;
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(count < maxCount)
+        {
+            SpawnRecyclable();
+        }
+	}
+
+    // 
+    void SpawnRecyclable()
+    {
+
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
