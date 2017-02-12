2b85a532a6d4b85eddf31a3e8f56a4a75e1a3f9c
Eric Kim <bosung90@gmail.com>
2017-02-12 10:21:02 -0800

    Wave moves

---
+++ Assets/EcoCat/Scripts/Wave.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	void Start () {
+		LeanTween.moveLocalX (this.gameObject, transform.position.x + 1, 2.3f)
+			.setEase(LeanTweenType.easeInOutCubic)
+			.setLoopPingPong (0);
+
+		LeanTween.moveLocalY (this.gameObject, transform.position.y + 0.1f, 3.7f)
+			.setEase(LeanTweenType.easeInOutCubic)
+			.setLoopPingPong (0);
+	}
+}
