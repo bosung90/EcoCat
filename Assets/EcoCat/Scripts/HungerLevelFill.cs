cf36bcdca35ef04c948a0e5de1d5ca4ba9d60e04
Eric Kim <bosung90@gmail.com>
2017-02-12 09:53:45 -0800

    Hunger level fill color changes based on its level. Hunger level bar is white at first.

--- Assets/EcoCat/Scripts/HungerLevelFill.cs
+++ Assets/EcoCat/Scripts/HungerLevelFill.cs
@@ -9,11 +9,18 @@ public class HungerLevelFill : MonoBehaviour {
 	public EcoCat ecoCat;
 	private Image image;
 
+	public Color full, mid, danger;
+
 	void Awake() {
 		image = GetComponent<Image> ();
 
 		ecoCat.HungerLevel.Subscribe (hungerLevel => {
 			image.fillAmount = hungerLevel;
+			if(hungerLevel>0.5) {
+				image.color = Color.Lerp(mid, full, (hungerLevel-0.5f) * 2);
+			} else {
+				image.color = Color.Lerp(danger, mid, (hungerLevel * 2f));
+			}
 		}).AddTo (this);
 	}
 
